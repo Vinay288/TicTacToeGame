5c5ec9e1d89769ff9ae91b702aa4dbca62092ea3
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 08:51:21 +0530

    ADDED:method to take player key as input

--- src/com/bridelabz/tictactoegame/TicTacToeMain.java
+++ src/com/bridelabz/tictactoegame/TicTacToeMain.java
@@ -1,10 +1,26 @@
 package com.bridelabz.tictactoegame;
 
+import java.util.Scanner;
+
 public class TicTacToeMain {
 
 	public static void main(String[] args) {
-		TicTacToeGame game=new TicTacToeGame();
+		char computerKey;
+		System.out.println("Welcome to Tic Tac Toe game");
+		char playerKey = selectKey();
+		if (playerKey == 'X') {
+			computerKey = 'O';
+		} else
+			computerKey = 'X';
+		TicTacToeGame game = new TicTacToeGame(playerKey, computerKey);
 
+	}
+
+	public static char selectKey() {
+		System.out.println("Player1 choose a letter X or O");
+		Scanner scanner = new Scanner(System.in);
+		char playerKey = scanner.next().charAt(0);
+		return playerKey;
 	}
 
 }
