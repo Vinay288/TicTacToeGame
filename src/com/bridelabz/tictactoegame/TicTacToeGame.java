4504945fe468e72f29dede49730a3380f7199a2e
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 08:50:51 +0530

    Modify:chnaged constructor to take playerkey as parameter

--- src/com/bridelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridelabz/tictactoegame/TicTacToeGame.java
@@ -2,8 +2,12 @@ package com.bridelabz.tictactoegame;
 
 public class TicTacToeGame {
 	char[] board;
+	private char playerKey;
+	private char computerKey;
 
-	public TicTacToeGame() {
+	public TicTacToeGame(char playerKey, char computerKey) {
+		this.playerKey = playerKey;
+		this.computerKey = computerKey;
 		board = new char[10];
 		assignInitialvalues();
 	}
