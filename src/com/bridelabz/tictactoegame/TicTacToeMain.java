93c146b56c520163a0d6f4c6108f41590a100f03
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 11:04:58 +0530

    Added selectUserKey method to take user input

--- src/com/bridelabz/tictactoegame/TicTacToeMain.java
+++ src/com/bridelabz/tictactoegame/TicTacToeMain.java
@@ -3,23 +3,26 @@ package com.bridelabz.tictactoegame;
 import java.util.Scanner;
 
 public class TicTacToeMain {
-
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
-		char computerKey;
 		System.out.println("Welcome to Tic Tac Toe game");
 		char playerKey = selectKey();
-		if (playerKey == 'X') {
-			computerKey = 'O';
-		} else
-			computerKey = 'X';
+		char computerKey=(playerKey=='X')?'O':'X';
 		TicTacToeGame game = new TicTacToeGame(playerKey, computerKey);
 		game.displayBoard();
+		gameStart(game);
+	}
 
+	public static void gameStart(TicTacToeGame game) {
+		System.out.println("enter the user move index (1-9) ");
+		int userMove=scanner.nextInt();
+		if(!game.playerGamePlay(userMove))
+			gameStart(game);
+		
 	}
 
 	public static char selectKey() {
 		System.out.println("Player1 choose a letter X or O");
-		Scanner scanner = new Scanner(System.in);
 		char playerKey = scanner.next().charAt(0);
 		return playerKey;
 	}
