6828dcebca3683b043a650f78039670296c033ce
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 09:36:42 +0530

    MODIFY:added pattern to show board

--- src/com/bridelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridelabz/tictactoegame/TicTacToeGame.java
@@ -15,15 +15,17 @@ public class TicTacToeGame {
 
 	private void assignInitialvalues() {
 		for (int i = 1; i < 10; i++) {
-			board[i] = ' ';
+			board[i] = 'A';
 		}
 	}
 
 	public void displayBoard() {
-		for (char i : board) {
-			System.out.println(i);
+		System.out.println("board values");
+		for (int i = 1; i < 10; i++) {
+			System.out.print(board[i] + "|");
+			if (i % 3 == 0) {
+				System.out.print("\n------\n");
+			}
 		}
-
 	}
-
 }
