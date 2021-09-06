25e55fbc785db5084c0ec3bcde01e4a97fe3f84c
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 16:48:36 +0530

    ADDED:computer play function which checks moves to win and plays accordingly

--- src/com/bridelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridelabz/tictactoegame/TicTacToeGame.java
@@ -1,5 +1,7 @@
 package com.bridelabz.tictactoegame;
 
+import java.util.Arrays;
+
 public class TicTacToeGame {
 	char[] board;
 	private char playerKey;
@@ -32,31 +34,43 @@ public class TicTacToeGame {
 	public boolean playerGamePlay(int userMove) {
 		if (board[userMove] == ' ') {
 			board[userMove] = playerKey;
-			System.out.println("ff");
 			return true;
 		}
-		
+
 		System.out.println("this box is already filled select different box");
 		return false;
 	}
 
 	public void computerPlay(TicTacToeGame game) {
-
-	}
-
-	public void gamePlayResult() {
-		if (isWin(playerKey)) {
-			System.out.println("player1 has won the game");
-		} else if (isWin(computerKey)) {
-			System.out.println("computer has wont the game");
-		} else {
-			System.out.println("change the turn");
+		int[] freePositons = getFreePositions();
+		for (int i = 1; i < 10; i++) {
+			if (freePositons[i] == 0) {
+				board[i] = computerKey;
+				if (gamePlayResult() == 2) {
+					return;
+				}
+				board[i] = ' ';
+			}
 		}
 
 	}
 
+	public int gamePlayResult() {
+		if (isWin(playerKey)) {
+			System.out.println("player1 has won the game");
+			return 1;
+		} else if (isWin(computerKey)) {
+			System.out.println("computer has wont the game");
+			return 2;
+		} else if (getFreePositions() == null) {
+			System.out.println("game has tied");
+			return 0;
+		}
+		System.out.println("change the turn");
+		return -1;
+	}
+
 	private boolean isWin(char key) {
-		System.out.println("d");
 		if (board[1] == key && board[5] == key && board[9] == key)
 			return true;
 		if (board[3] == key && board[5] == key && board[7] == key)
@@ -68,12 +82,28 @@ public class TicTacToeGame {
 			}
 			if (i % 3 == 0)
 				return true;
-			i+=1;
+			i += 1;
 		}
 		for (int i = 1; i < 4; i++) {
 			if (board[i] == key && board[i + 3] == key && board[i + 6] == key)
 				return true;
 		}
 		return false;
+	}
+
+	public int[] getFreePositions() {
+		int[] freePositions = new int[10];
+		boolean full = true;
+		Arrays.fill(freePositions, 1);
+		freePositions[0] = -1;
+		for (int i = 1; i < 10; i++) {
+			if (board[i] == ' ') {
+				full = false;
+				freePositions[i] = 0;
+			}
+		}
+		if (full)
+			return null;
+		return freePositions;
 	}
 }
