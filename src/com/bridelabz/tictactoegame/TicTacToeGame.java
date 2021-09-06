157f6f543d08bf600873ebf56c5352f84eadf745
Vinay288 <vinayhiremath288@gmail.com>
2021-09-06 12:13:19 +0530

    ADDED:method to find the winner or tie game

--- src/com/bridelabz/tictactoegame/TicTacToeGame.java
+++ src/com/bridelabz/tictactoegame/TicTacToeGame.java
@@ -30,18 +30,50 @@ public class TicTacToeGame {
 	}
 
 	public boolean playerGamePlay(int userMove) {
-		if(board[userMove]==' ') {
-			board[userMove]=playerKey;
+		if (board[userMove] == ' ') {
+			board[userMove] = playerKey;
+			System.out.println("ff");
 			return true;
 		}
-		else {
-			System.out.println("this box is already filled select different box");
-		}
+		
+		System.out.println("this box is already filled select different box");
 		return false;
 	}
 
 	public void computerPlay(TicTacToeGame game) {
-	
-		
+
+	}
+
+	public void gamePlayResult() {
+		if (isWin(playerKey)) {
+			System.out.println("player1 has won the game");
+		} else if (isWin(computerKey)) {
+			System.out.println("computer has wont the game");
+		} else {
+			System.out.println("change the turn");
+		}
+
+	}
+
+	private boolean isWin(char key) {
+		System.out.println("d");
+		if (board[1] == key && board[5] == key && board[9] == key)
+			return true;
+		if (board[3] == key && board[5] == key && board[7] == key)
+			return true;
+		for (int i = 1; i < 10;) {
+			if (board[i] != key) {
+				i += 3;
+				break;
+			}
+			if (i % 3 == 0)
+				return true;
+			i+=1;
+		}
+		for (int i = 1; i < 4; i++) {
+			if (board[i] == key && board[i + 3] == key && board[i + 6] == key)
+				return true;
+		}
+		return false;
 	}
 }
